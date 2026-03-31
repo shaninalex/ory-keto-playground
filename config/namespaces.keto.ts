95eba295caefb6dcae8b0c9bf4821f2b01458745
shaninalex <shanin.x@gmail.com>
2026-03-31 08:05:40 +0300

    dev

---
+++ config/namespaces.keto.ts
@@ -0,0 +1,12 @@
+// Copyright © 2023 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+import { Namespace, SubjectSet, Context } from "@ory/keto-namespace-types"
+
+class User implements Namespace {}
+
+class Role implements Namespace {
+  related: {
+    member: User[]
+  }
+}
